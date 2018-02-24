2961b4cb4ab4cc091f61bc762835fc37f1d25cbf
juno2myth <juno2myth@gmail.com>
2018-02-24 18:47:59 +0900

    update BTCpair

--- SymbolBTC.py
+++ SymbolBTC.py
@@ -1,14 +1,37 @@
-# 銘柄ペア
+# BTCペア
+ada = 'ADABTC'
+adx = 'ADXBTC'
+ae = 'AEBTC'
+aion = 'AIONBTC'
+amb = 'AMBBTC'
+appc = 'APPCBTC'
+ark = 'ARKBTC'
+arn = 'ARNBTC'
+ast = 'ASTBTC'
+bat = 'BATBTC'
+bcc = 'BCCBTC'
+bcd = 'BCDBTC'
+bcpt = 'BCPTBTC'
+blz = 'BLZBTC'
+bnb = 'BNBBTC'
+bnt = 'BNTBTC'
+bqx = 'BQXBTC'
+brd = 'BRDBTC'
+btg = 'BTGBTC'
+bts = 'BTSBTC'
+
+cnd = 'CNDBTC'
+
+eth = 'ETHBTC'
+
+lsk = 'LSKBTC'
+lun = 'LUNBTC'
+
+neo = 'NEOBTC'
 
 trx = 'TRXBTC'
-lsk = 'LSKBTC'
-eth = 'ETHBTC'
+
 ven = 'VENBTC'
 
 xrp = 'XRPBTC'
-ada = 'ADABTC'
-cnd = 'CNDBTC'
-bcc = 'BCCBTC'
-neo = 'NEOBTC'
-lun = 'LUNBTC'
 
